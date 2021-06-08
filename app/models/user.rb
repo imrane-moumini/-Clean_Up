c861839d394513c982b66e06b89f364ac265cf35
Margaux Rouanet <margaux.rouanet@kedgebs.com>
2021-06-08 16:01:44 +0200

    cloud qui marche

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many_attached :photos
+  has_one_attached :photo
+  has_one_attached :degree_file
   has_many :slots
   has_many :bookings
   has_many :reviews
