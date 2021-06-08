f0258889b13e7b008a8686f312b981598f6cd6a2
Imrane Moumini <imranemouminipro@gmail.com>
2021-06-08 14:50:36 +0200

    m

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,2 +1,8 @@
 class ReviewController < ApplicationController
+  def new
+     authorize @review
+  end
+  def create
+    authorize @review
+  end
 end
--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -1,2 +1,21 @@
 class SlotsController < ApplicationController
+  def new
+     authorize @slot
+  end
+
+  def create
+    authorize @slot
+  end
+
+  def edit
+    authorize @slot
+  end
+
+  def update
+    authorize @slot
+  end
+
+  def destroy
+    authorize @slot
+  end
 end
