35abe9880de0814204e8e293d1fc3a3c98967861
Imrane Moumini <imranemouminipro@gmail.com>
2021-06-10 17:17:41 +0200

    new fini

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,25 @@
 class BookingsController < ApplicationController
-  def new?
-    authorize @bookings
+  def new
 
     #@dashboard = Dashboard.find(params[:dashboard_id])
     @booking = Booking.new
+    authorize @booking
+
+    @ville = params[:ville]
+    @start_query = params[:start_time]
+    @end_query = params[:end_time]
+
+    if @ville.present? && @start_query.present? && @end_query.present?
+      @slots = Slot.joins(:user).where("address ILIKE ?", "%#{params[:ville]}%")
+      @slots.select do |slot|
+        slot.start_time <= @start_query && slot.end_time >= @end_query
+      end
+      @available_slots = @slots.select do |slot|
+        slot.available?(@start_query, @end_query)
+      end
+    end
+
+    @available_slot_one = @available_slots.sample
   end
 
   def show
@@ -13,14 +29,16 @@ class BookingsController < ApplicationController
   end
 
   def create
-    authorize @bookings
 
     @booking = Booking.new(booking_params)
+    authorize @booking
     #@dashboard = Dashboard.find(params[:dashboard_id])
     @booking.user = current_user
     #@booking.dashboard = @dashboard
+    @booking.booking_price =  ( ((@booking.end_time - @booking.start_time)/3600) * @booking.user.user_price )
+
       if @booking.save
-        redirect_to dashboard_path(@dashboard)
+         redirect_to booking_path(@booking)
       else
         render :new
       end
@@ -37,6 +55,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:start_time, :status, :end_time)
+    params.require(:booking).permit(:start_time, :status, :end_time, :slot_id, :task_accomplished)
   end
 end
