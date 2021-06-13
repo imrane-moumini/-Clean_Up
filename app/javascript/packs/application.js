076b973cd43d193f115c7b2543423594745d8a09
Imrane Moumini <imranemouminipro@gmail.com>
2021-06-13 16:18:08 +0200

    marche pas

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,7 @@ require("channels")
 // External imports
 import "bootstrap";
 import { initToggle } from '../components/init_toggle_pay';
+import { initChatroomCable } from '../channels/chatroom_channel';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
@@ -31,4 +32,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initToggle();
+  initChatroomCable();
 });
