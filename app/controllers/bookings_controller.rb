e4f2c56e3bed34927f01162af9f26c4a9a6ff181
Imrane Moumini <imranemouminipro@gmail.com>
2021-06-16 09:23:24 +0200

    sécurité

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -48,7 +48,7 @@ class BookingsController < ApplicationController
           quantity: 1
         }],
         success_url: booking_url(@booking),
-        cancel_url: booking_url(@booking)
+        cancel_url: dashboard_url
       })
       @booking.update(checkout_session_id: session.id)
       redirect_to new_booking_payment_path(@booking)
